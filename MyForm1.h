bc26bd1b84b58a651c833e5b10915e2e20724982
boundj <boundj@DESKTOP-LKBMPNO>
2020-11-29 11:08:33 +0100

    abdou03

--- MyForm1.h
+++ MyForm1.h
@@ -1,4 +1,5 @@
 #pragma once
+using namespace System::Data::SqlClient;
 
 namespace gestion {
 
@@ -8,7 +9,7 @@ namespace gestion {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
-	using namespace System::Data::SqlClient;
+
 
 
 	/// <summary>
@@ -45,20 +46,33 @@ namespace gestion {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Label^ label4;
-	private: System::Windows::Forms::TextBox^ textBox2;
+
 	private: System::Windows::Forms::Label^ label5;
-	private: System::Windows::Forms::TextBox^ textBox3;
+
 	private: System::Windows::Forms::Label^ label6;
 	private: System::Windows::Forms::Label^ label7;
 	private: System::Windows::Forms::Label^ label8;
-	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::TextBox^ textBox4;
-	private: System::Windows::Forms::TextBox^ textBox5;
-	private: System::Windows::Forms::TextBox^ textBox6;
+
+
+
+
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::TextBox^ textBox6;
+	private: System::Windows::Forms::BindingSource^ bindingSource1;
+	private: System::Windows::Forms::DataGridView^ dataGridView2;
+	private: System::Windows::Forms::TextBox^ textBox7;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::BindingSource^ bindingSource2;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::ComponentModel::IContainer^ components;
 
 
 
@@ -93,7 +107,7 @@ namespace gestion {
 		/// <summary>
 		/// Variable nécessaire au concepteur.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -102,26 +116,36 @@ namespace gestion {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm1::typeid));
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->label5 = (gcnew System::Windows::Forms::Label());
-			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->label6 = (gcnew System::Windows::Forms::Label());
 			this->label7 = (gcnew System::Windows::Forms::Label());
 			this->label8 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->bindingSource1 = (gcnew System::Windows::Forms::BindingSource(this->components));
+			this->dataGridView2 = (gcnew System::Windows::Forms::DataGridView());
+			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->bindingSource2 = (gcnew System::Windows::Forms::BindingSource(this->components));
+			this->button5 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource2))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -171,13 +195,6 @@ namespace gestion {
 			this->label4->TabIndex = 10;
 			this->label4->Text = L"Date du premier achat";
 			// 
-			// textBox2
-			// 
-			this->textBox2->Location = System::Drawing::Point(12, 214);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(143, 20);
-			this->textBox2->TabIndex = 11;
-			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
@@ -190,14 +207,6 @@ namespace gestion {
 			this->label5->Size = System::Drawing::Size(113, 14);
 			this->label5->TabIndex = 12;
 			this->label5->Text = L"Adresse Livraison";
-			// 
-			// textBox3
-			// 
-			this->textBox3->Location = System::Drawing::Point(12, 163);
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(143, 20);
-			this->textBox3->TabIndex = 13;
-			this->textBox3->TextChanged += gcnew System::EventHandler(this, &MyForm1::textBox3_TextChanged);
 			// 
 			// label6
 			// 
@@ -239,34 +248,6 @@ namespace gestion {
 			this->label8->TabIndex = 18;
 			this->label8->Text = L"Prenom";
 			// 
-			// textBox1
-			// 
-			this->textBox1->Location = System::Drawing::Point(12, 312);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(143, 20);
-			this->textBox1->TabIndex = 19;
-			// 
-			// textBox4
-			// 
-			this->textBox4->Location = System::Drawing::Point(12, 118);
-			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(143, 20);
-			this->textBox4->TabIndex = 20;
-			// 
-			// textBox5
-			// 
-			this->textBox5->Location = System::Drawing::Point(12, 76);
-			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(143, 20);
-			this->textBox5->TabIndex = 21;
-			// 
-			// textBox6
-			// 
-			this->textBox6->Location = System::Drawing::Point(12, 261);
-			this->textBox6->Name = L"textBox6";
-			this->textBox6->Size = System::Drawing::Size(143, 20);
-			this->textBox6->TabIndex = 22;
-			// 
 			// dataGridView1
 			// 
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
@@ -283,6 +264,7 @@ namespace gestion {
 			this->button2->TabIndex = 24;
 			this->button2->Text = L"Supprimer";
 			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm1::button2_Click);
 			// 
 			// button3
 			// 
@@ -292,6 +274,7 @@ namespace gestion {
 			this->button3->TabIndex = 25;
 			this->button3->Text = L"Afficher";
 			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm1::button3_Click);
 			// 
 			// button4
 			// 
@@ -301,6 +284,83 @@ namespace gestion {
 			this->button4->TabIndex = 26;
 			this->button4->Text = L"Modifier";
 			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm1::button4_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(12, 76);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(142, 20);
+			this->textBox1->TabIndex = 27;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(13, 118);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(142, 20);
+			this->textBox2->TabIndex = 28;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(12, 163);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(142, 20);
+			this->textBox3->TabIndex = 29;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(13, 214);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(142, 20);
+			this->textBox4->TabIndex = 30;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(13, 260);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(142, 20);
+			this->textBox5->TabIndex = 31;
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(12, 312);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(142, 20);
+			this->textBox6->TabIndex = 32;
+			// 
+			// dataGridView2
+			// 
+			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView2->Location = System::Drawing::Point(184, 59);
+			this->dataGridView2->Name = L"dataGridView2";
+			this->dataGridView2->Size = System::Drawing::Size(414, 221);
+			this->dataGridView2->TabIndex = 33;
+			// 
+			// textBox7
+			// 
+			this->textBox7->Location = System::Drawing::Point(442, 33);
+			this->textBox7->Name = L"textBox7";
+			this->textBox7->Size = System::Drawing::Size(141, 20);
+			this->textBox7->TabIndex = 34;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(442, 14);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(50, 13);
+			this->label2->TabIndex = 35;
+			this->label2->Text = L"ID_Client";
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(34, 14);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(75, 23);
+			this->button5->TabIndex = 36;
+			this->button5->Text = L"afficher tout ";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &MyForm1::button5_Click);
 			// 
 			// MyForm1
 			// 
@@ -309,20 +369,24 @@ namespace gestion {
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			this->ClientSize = System::Drawing::Size(610, 350);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox7);
+			this->Controls->Add(this->dataGridView2);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->dataGridView1);
-			this->Controls->Add(this->textBox6);
-			this->Controls->Add(this->textBox5);
-			this->Controls->Add(this->textBox4);
-			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label8);
 			this->Controls->Add(this->label7);
 			this->Controls->Add(this->label6);
-			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->label5);
-			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label1);
@@ -331,6 +395,9 @@ namespace gestion {
 			this->Text = L"MyForm1";
 			this->Load += gcnew System::EventHandler(this, &MyForm1::MyForm1_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource2))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -338,17 +405,143 @@ namespace gestion {
 #pragma endregion
 	private: System::Void MyForm1_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
-	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
-private: System::Void textBox3_TextChanged(System::Object^ sender, System::EventArgs^ e) {
-}
-private: System::Void label6_Click(System::Object^ sender, System::EventArgs^ e) {
-}
-private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
-}
-private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	String^ constring = "Data Source=(local);Initial Catalog=POO;Integrated Security=True";
-	SqlConnection^ con = gcnew SqlConnection(constring);
-}
-};
+	private: System::Void label6_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+	}
+
+
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ constring = "Data Source=(local);Initial Catalog=POO_PROJET;Integrated Security=True";
+		SqlConnection^ condatabase = gcnew SqlConnection(constring);
+
+		String^ nom = textBox1->Text;
+		String^ prenom = textBox2->Text;
+		String^ adresseL = textBox3->Text;
+		String^ adresseF = textBox4->Text;
+		String^ date = Convert::ToDateTime(textBox5->Text).ToString("yyyy-MM-dd");
+		String^ date2 = Convert::ToDateTime(textBox6->Text).ToString("dd-MM-yyyy");
+
+		SqlCommand^ cmdDataBase = gcnew SqlCommand("INSERT INTO  client (Nom_c, Prenom_c , Date_De_Naissaince , Date_Premier_Achat, Adresse_Livraison, Adresse_Facturation ) values('" + nom + "','" + prenom + "','" + date + "','" + date2 + "','" + adresseF + "','" + adresseL + "');", condatabase);
+		SqlDataReader^ myReader;
+		try {
+			condatabase->Open();
+			myReader = cmdDataBase->ExecuteReader();
+			MessageBox::Show("client  enregistré :'D");
+		}
+		catch (Exception^ ex) {
+			MessageBox::Show(ex->Message);
+		}
+
+
+
+	}
+
+
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+
+		String^ constring = "Data Source=(local);Initial Catalog=POO_PROJET;Integrated Security=True";
+		SqlConnection^ conDataBase = gcnew SqlConnection(constring);
+
+		String^ ID = textBox7->Text;
+		SqlCommand^ cmdDataBase = gcnew SqlCommand("DELETE FROM client WHERE Num_c = '" + ID + "' ", conDataBase);
+
+		conDataBase->Open();
+		SqlDataReader^ myReader = cmdDataBase->ExecuteReader();
+		MessageBox::Show("Client  supprimé :'D");
+		conDataBase->Close();
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+
+
+		String^ reference = textBox7->Text;
+		String^ constring = "Data Source=(local);Initial Catalog=POO_PROJET;Integrated Security=True";
+		SqlConnection^ conDataBase = gcnew SqlConnection(constring);
+		SqlCommand^ cmdDataBase = gcnew SqlCommand("SELECT * FROM client WHERE Num_c = '" + reference + "' ", conDataBase);
+		conDataBase->Open();
+		SqlDataReader^ myReader = cmdDataBase->ExecuteReader();
+		dataGridView1->Hide();
+		dataGridView2->Show();
+
+		while (myReader->Read()) {
+			textBox1->Text = myReader->GetString(1);
+			textBox2->Text = myReader->GetString(2);
+			textBox3->Text = myReader->GetString(5);
+			textBox4->Text = myReader->GetString(6);
+			textBox5->Text = Convert::ToString(myReader->GetDateTime(3).ToString("yyyy - MM - dd"));
+			textBox6->Text = Convert::ToString(myReader->GetDateTime(4).ToString("yyyy - MM - dd"));
+		}
+		myReader->Close();
+		SqlDataAdapter^ adapter = gcnew SqlDataAdapter("SELECT * FROM client WHERE Num_c = '" + reference + "' ", conDataBase);
+		DataTable^ data = gcnew DataTable();
+		data->Clear();
+		adapter->Fill(data);
+		bindingSource2->DataSource = data;
+		dataGridView2->DataSource = bindingSource2;
+
+	}
+
+
+
+
+
+
+	private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+
+		String^ constring = "Data Source=(local);Initial Catalog=POO_PROJET;Integrated Security=True";
+		SqlConnection^ condatabase = gcnew SqlConnection(constring);
+		String^ id = textBox7->Text;
+		String^ nom = textBox1->Text;
+		String^ prenom = textBox2->Text;
+		String^ adresseL = textBox3->Text;
+		String^ adresseF = textBox4->Text;
+		String^ date = Convert::ToDateTime(textBox5->Text).ToString("yyyy-MM-dd");
+		String^ date2 = Convert::ToDateTime(textBox6->Text).ToString("dd-MM-yyyy");
+
+		SqlCommand^ cmdDataBase = gcnew SqlCommand("UPDATE client SET  Nom_c = '" + nom + "', Prenom_c ='" + prenom + "' , Date_De_Naissaince = '" + date + "' , Date_Premier_Achat = '" + date2 + "', Adresse_Livraison = '" + adresseF + "', Adresse_Facturation = '" + adresseL + "' WHERE Num_c = " + id + " ;", condatabase);
+		SqlDataReader^ myReader;
+		try {
+			condatabase->Open();
+			myReader = cmdDataBase->ExecuteReader();
+			MessageBox::Show("client modifié :'D");
+		}
+		catch (Exception^ ex) {
+			MessageBox::Show(ex->Message);
+		}
+
+
+
+	}
+
+
+
+
+
+	private: System::Void button5_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ constring = "Data Source=(local);Initial Catalog=POO_PROJET;Integrated Security=True";
+		SqlConnection^ conDataBase = gcnew SqlConnection(constring);
+		SqlDataAdapter^ adapter = gcnew SqlDataAdapter("SELECT * FROM client", conDataBase);
+		DataTable^ data = gcnew DataTable();
+		dataGridView2->Hide();
+		dataGridView1->Show();
+		adapter->Fill(data);
+		bindingSource1->DataSource = data;
+		dataGridView1->DataSource = bindingSource1;
+	}
+	};
 }
